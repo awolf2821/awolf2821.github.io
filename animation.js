f24b050713025070b6a71bd79fffd3963cc064ae
Jackson Shartzer <jacksonshartzer@gmail.com>
2025-04-29 08:14:50 -0400

    Updated Animation

--- animation.js
+++ animation.js
@@ -8,37 +8,66 @@ let dY = 5;
 let xInvert = false;
 let yInvert = false;
 
+const player={
+    //variables use key:value pair syntax
+    x:200,
+    y:200,
+    color:"Green",
+    speed:3
+}
+
 //define functions
 function drawRect(x,y) {
-    console.log("drawing rect");
+    //console.log("drawing rect");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'blue';
     ctx.fillRect(x,y,50,50);
     ctx.fill();
 }
 
+function drawPlayer(){
+    ctx.fillStyle = player.color;
+    ctx.beginPath();
+    ctx.arc(
+        player.x,
+        player.y,
+        20,
+        0, 
+        2 * Math.PI);
+        ctx.fill();
+}
+
 function animate() {
     //Inversion Detection and Logic:
-    if(x>canvas.width){
-        xInvert = true;
-        dX = -(dX);
+    if(x>1920){
+        dX = dX * -1;
     }
     if(x<0){
-        xInvert = false;
-        dX = Math.fabs(dX);
+        dX = dX * -1;
     }
-    if(y>canvas.height){
-        yInvert = true;
-        dY = -(dY);
+    if(y>1080){
+        dY = dY * -1;
     }
     if(y<0){
-        yInvert = false;
-        dY = Math.fabs(dY);
+        dY = dY * -1;
     }
+    //In order to handle an event we need two things:
+        //-Event handler, does things because event
+        //-Event listener, notices when an event happens and calls the handler
+
     x = x+dX;
     y = y+dY;
 
+    function handleKeyPress(e){
+        console.log(e.key);
+    }
+
+    document.addEventListener('keydown', handleKeyPress);
+
     requestAnimationFrame(animate);
+    drawRect(x,y);
+    drawPlayer();
+
 }
 
 //call our function
